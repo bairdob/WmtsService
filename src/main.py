dd37dd01c67a6af013e63c8e73fe963227c4b5c6
bairdob <krumane@mail.ru>
2023-07-24 20:35:40 +0300

    refactor

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import hashlib
 import sqlite3
 import traceback
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status
 from fastapi.exceptions import HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response, JSONResponse
@@ -44,40 +44,43 @@ async def exception_handler(request, exc):
         'traceback': traceback.format_exc(),
         'url': request.url._url,
     }
-    return JSONResponse(status_code=500, content=error_response)
+    return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content=error_response
+    )
 
 
 @app.get("/wmts", response_class=Response)
 async def get_tile(layer: str,
                    style: str,
                    tilematrixset: str,
-                   Service: str,
-                   Request: str,
-                   Version: str,
-                   Format: str,
-                   TileMatrix: int,
-                   TileCol: int,
-                   TileRow: int,
-                   request: Request) -> Response:
+                   service: str,
+                   request: str,
+                   version: str,
+                   format: str,
+                   tilematrix: int,
+                   tilecol: int,
+                   tilerow: int,
+                   _request: Request) -> Response:
     """Получаем тайл из БД."""
 
-    if Service != 'WMTS' or Request != 'GetTile':
+    if service != 'wmts' or request != 'gettile':
         return Response(
-            content=f'Invalid request: {request.url._url}',
-            status_code=400)
+            content=f'Invalid request: {_request.url._url}',
+            status_code=status.HTTP_400_BAD_REQUEST)
 
     if not layer.startswith('/'):
         layer = '/' + layer
 
     # инвертируем y
-    TileRow = (1 << TileMatrix) - TileRow - 1
+    tilerow = (1 << tilematrix) - tilerow - 1
 
     # получаем путь к БД из параметра layer
     try:
         db_path = get_first_file_in_folder(layer, MBTiles.SUFFIX.value)
     except FileNotFoundError:
         raise HTTPException(
-            status_code=404,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail=f'File: {layer}*{MBTiles.SUFFIX.value} not found'
         ) from None
 
@@ -85,11 +88,11 @@ async def get_tile(layer: str,
     async with AsyncSQLite(db_path) as db:
         try:
             tile = await db.get_db_data(
-                MBTiles.get_tile_query(z=TileMatrix, x=TileCol, y=TileRow))
+                MBTiles.get_tile_query(z=tilematrix, x=tilecol, y=tilerow))
         except sqlite3.OperationalError as e:
             raise HTTPException(
-                status_code=404,
-                detail=f"Tile: {TileMatrix}, {TileCol}, {TileRow} not found"
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Tile: {tilematrix}, {tilecol}, {tilerow} not found"
             ) from e
 
     return Response(
